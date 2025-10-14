7a905303798778201bcc2490f084b5885a6117ff
Julio Philippus <juliogerald25@gmail.com>
2025-10-14 15:44:11 +0700

    add: update genre

--- src/controllers/genreController.ts
+++ src/controllers/genreController.ts
@@ -22,7 +22,7 @@ export const getGenres = async (req: Request, res: Response) => {
   }
 };
 
-export const postGenre = async (req: Request, res: Response) => {
+export const createGenre = async (req: Request, res: Response) => {
   try {
     const body = genreSchema.parse(req.body);
 
@@ -37,6 +37,41 @@ export const postGenre = async (req: Request, res: Response) => {
       message: "Genre created successfully",
       data: {
         genre: createdGenre,
+      },
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      status: "error",
+      message: error.message || "Internal server error",
+      data: null,
+    });
+  }
+};
+
+export const updateGenre = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const body = genreSchema.parse(req.body);
+
+    const genre = await Genre.findByIdAndUpdate(id, {
+      name: body.name,
+    });
+
+    if (!genre) {
+      return res.status(404).json({
+        status: "error",
+        message: "Genre not found",
+        data: null,
+      });
+    }
+
+    const updatedGenre = await Genre.findById(id);
+
+    res.status(200).json({
+      status: "success",
+      message: "Genre updated successfully",
+      data: {
+        genre: updatedGenre,
       },
     });
   } catch (error: any) {
--- src/routes/adminRoutes/genreRoutes.ts
+++ src/routes/adminRoutes/genreRoutes.ts
@@ -1,11 +1,16 @@
 import express from "express";
-import { getGenres, postGenre } from "../../controllers/genreController";
+import {
+  getGenres,
+  createGenre,
+  updateGenre,
+} from "../../controllers/genreController";
 import { validateRequest } from "../../middlewares/validateRequest";
 import { genreSchema } from "../../utils/zodSchema";
 
 const genreRouter = express.Router();
 
 genreRouter.get("/", getGenres);
-genreRouter.post("/", validateRequest(genreSchema), postGenre);
+genreRouter.post("/", validateRequest(genreSchema), createGenre);
+genreRouter.put("/:id", validateRequest(genreSchema), updateGenre);
 
 export default genreRouter;
